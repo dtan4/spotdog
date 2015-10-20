49319f7adeb04f68f3ca21bf091ee8520b73200b
Daisuke Fujita <dtanshi45@gmail.com>
2015-10-20 12:14:41 +0900

    Change test fixture scope

--- spec/lib/spotdog/datadog_spec.rb
+++ spec/lib/spotdog/datadog_spec.rb
@@ -14,56 +14,11 @@ module Spotdog
       described_class.new(api_key, prefix)
     end
 
-    let(:c4xlarge_linux_vpc_1b_1) do
-      {
-        instance_type: "c4.xlarge",
-        product_description: "Linux/UNIX (Amazon VPC)",
-        spot_price: "0.143600",
-        timestamp: Time.parse("2015-10-06 05:39:52 UTC"),
-        availability_zone: "ap-northeast-1b",
-      }
-    end
-
-    let(:c4xlarge_linux_vpc_1b_2) do
-      {
-        instance_type: "c4.xlarge",
-        product_description: "Linux/UNIX (Amazon VPC)",
-        spot_price: "0.233600",
-        timestamp: Time.parse("2015-10-06 05:29:52 UTC"),
-        availability_zone: "ap-northeast-1b",
-      }
-    end
-
-    let(:m4large_windows_classic_1c) do
-      {
-        instance_type: "m4.large",
-        product_description: "Windows",
-        spot_price: "1.143600",
-        timestamp: Time.parse("2015-10-06 05:20:52 UTC"),
-        availability_zone: "ap-northeast-1c",
-      }
-    end
-
-    let(:r3xlarge_suse_vpc_1c) do
-      {
-        instance_type: "r3.xlarge",
-        product_description: "SUSE Linux (Amazon VPC)",
-        spot_price: "1.343600",
-        timestamp: Time.parse("2015-10-06 05:10:52 UTC"),
-        availability_zone: "ap-northeast-1c",
-      }
-    end
-
-    let(:spot_price_history) do
-      [
-        c4xlarge_linux_vpc_1b_1,
-        c4xlarge_linux_vpc_1b_2,
-        m4large_windows_classic_1c,
-        r3xlarge_suse_vpc_1c,
-      ]
-    end
-
     describe ".send_price_history" do
+      let(:spot_price_history) do
+        []
+      end
+
       before do
         allow_any_instance_of(described_class).to receive(:send_price_history).and_return([])
       end
@@ -75,6 +30,55 @@ module Spotdog
     end
 
     describe "#send_price_history" do
+      let(:c4xlarge_linux_vpc_1b_1) do
+        {
+          instance_type: "c4.xlarge",
+          product_description: "Linux/UNIX (Amazon VPC)",
+          spot_price: "0.143600",
+          timestamp: Time.parse("2015-10-06 05:39:52 UTC"),
+          availability_zone: "ap-northeast-1b",
+        }
+      end
+
+      let(:c4xlarge_linux_vpc_1b_2) do
+        {
+          instance_type: "c4.xlarge",
+          product_description: "Linux/UNIX (Amazon VPC)",
+          spot_price: "0.233600",
+          timestamp: Time.parse("2015-10-06 05:29:52 UTC"),
+          availability_zone: "ap-northeast-1b",
+        }
+      end
+
+      let(:m4large_windows_classic_1c) do
+        {
+          instance_type: "m4.large",
+          product_description: "Windows",
+          spot_price: "1.143600",
+          timestamp: Time.parse("2015-10-06 05:20:52 UTC"),
+          availability_zone: "ap-northeast-1c",
+        }
+      end
+
+      let(:r3xlarge_suse_vpc_1c) do
+        {
+          instance_type: "r3.xlarge",
+          product_description: "SUSE Linux (Amazon VPC)",
+          spot_price: "1.343600",
+          timestamp: Time.parse("2015-10-06 05:10:52 UTC"),
+          availability_zone: "ap-northeast-1c",
+        }
+      end
+
+      let(:spot_price_history) do
+        [
+          c4xlarge_linux_vpc_1b_1,
+          c4xlarge_linux_vpc_1b_2,
+          m4large_windows_classic_1c,
+          r3xlarge_suse_vpc_1c,
+        ]
+      end
+
       let(:c4xlarge_points) do
         [
           [Time.parse("2015-10-06 05:39:52 UTC"), 0.1436],
