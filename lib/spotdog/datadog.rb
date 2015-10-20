626a1d73de2481cfab156e356415ef86de919589
Daisuke Fujita <dtanshi45@gmail.com>
2015-10-20 13:41:33 +0900

    Send spot instance requests to Datadog

--- lib/spotdog/datadog.rb
+++ lib/spotdog/datadog.rb
@@ -1,9 +1,14 @@
 module Spotdog
   class Datadog
     DEFAULT_PREFIX = "spotinstance"
+    STATUS_PREFIX = "status"
 
     def self.send_price_history(api_key, spot_price_history, prefix: DEFAULT_PREFIX)
       self.new(api_key, prefix).send_price_history(spot_price_history)
+    end
+
+    def self.send_spot_instance_requests(api_key, spot_instance_requests, prefix: DEFAULT_PREFIX)
+      self.new(api_key, prefix).send_spot_instance_requests(spot_instance_requests)
     end
 
     def initialize(api_key, prefix)
@@ -12,27 +17,43 @@ module Spotdog
     end
 
     def send_price_history(spot_price_history)
-      groups_from(spot_price_history).each { |metric_name, price_history| @client.emit_points(metric_name, points_of(price_history)) }
+      price_groups_from(spot_price_history).each { |metric_name, price_history| @client.emit_points(metric_name, price_points_of(price_history)) }
+    end
+
+    def send_spot_instance_requests(spot_instance_requests)
+      request_groups_from(spot_instance_requests).each { |metric_name, requests| @client.emit_points(metric_name, request_points_of(requests)) }
     end
 
     private
 
-    def groups_from(spot_price_history)
+    def price_groups_from(spot_price_history)
       spot_price_history.inject({}) do |result, spot_price|
-        metric_name = metric_name_of(spot_price)
+        metric_name = price_metric_name_of(spot_price)
         result[metric_name] ||= []
         result[metric_name] << spot_price
         result
       end
     end
 
-    def metric_name_of(spot_price)
+    def request_groups_from(spot_instance_requests)
+      spot_instance_requests.group_by { |request| request_metric_name_of(request) }
+    end
+
+    def price_metric_name_of(spot_price)
       # "spotinstance.c4_xlarge.linux_vpc.ap-northeast-1b"
       [
         @prefix,
         spot_price[:instance_type].sub(".", "_"),
         os_type_of(spot_price),
         spot_price[:availability_zone].gsub("-", "_")
+      ].join(".")
+    end
+
+    def request_metric_name_of(spot_instance_request)
+      [
+        @prefix,
+        STATUS_PREFIX,
+        spot_instance_request[:state],
       ].join(".")
     end
 
@@ -55,8 +76,13 @@ module Spotdog
       "#{machine_os_of(spot_price)}_#{machine_type_of(spot_price)}"
     end
 
-    def points_of(spot_price_history)
+    def price_points_of(spot_price_history)
       spot_price_history.map { |spot_price| [spot_price[:timestamp], spot_price[:spot_price].to_f] }
+    end
+
+    def request_points_of(requests)
+      current_time = Time.now
+      requests.map { |request| [current_time, requests.length] }
     end
   end
 end
--- spec/lib/spotdog/datadog_spec.rb
+++ spec/lib/spotdog/datadog_spec.rb
@@ -29,6 +29,21 @@ module Spotdog
       end
     end
 
+    describe ".send_spot_instance_requests" do
+      let(:spot_instance_requests) do
+        []
+      end
+
+      before do
+        allow_any_instance_of(described_class).to receive(:send_spot_instance_requests).and_return([])
+      end
+
+      it "should create new #{described_class} isntance and call #send_spot_instance_requests" do
+        expect_any_instance_of(described_class).to receive(:send_spot_instance_requests)
+        described_class.send_spot_instance_requests(api_key, spot_instance_requests)
+      end
+    end
+
     describe "#send_price_history" do
       let(:c4xlarge_linux_vpc_1b_1) do
         {
@@ -116,6 +131,81 @@ module Spotdog
           r3xlarge_points
         )
         datadog.send_price_history(spot_price_history)
+      end
+    end
+
+    describe "#send_spot_instance_requests" do
+      let(:spot_instance_request) do
+        {
+          spot_instance_request_id: "sir-1234abcd",
+          spot_price: "0.051120",
+          type: "one-time",
+          state: "active",
+          status: {
+            code: "fulfilled", update_time: Time.parse("2015-10-20 12:34:56 UTC"), message: "Your Spot request is fulfilled."
+          },
+          launch_specification: {
+            image_id: "ami-1234abcd",
+            key_name: "hoge",
+            security_groups: [
+              { group_name: "default", group_id: "sg-1234abcd" }
+            ],
+            instance_type: "c4.xlarge",
+            placement: { availability_zone: "ap-northeast-1c" },
+            block_device_mappings: [
+              {
+                device_name: "/dev/xvda", ebs: {
+                  volume_size: 50, delete_on_termination: true, volume_type: "gp2"
+                }
+              },
+              {
+                device_name: "/dev/xvdb", no_device: ""
+              }
+            ],
+            network_interfaces: [
+              { device_index: 0, subnet_id: "subnet-1234abcd", associate_public_ip_address: true }
+            ],
+            ebs_optimized: true,
+            monitoring: { enabled: false }
+          },
+          instance_id: "i-1234abcd",
+          create_time: Time.parse("2015-10-20 12:34:56 UTC"),
+          product_description: "Linux/UNIX",
+          tags: [
+            { key: "Name", value: "" }
+          ],
+          launched_availability_zone: "ap-northeast-1c"
+        }
+      end
+
+      let(:spot_instance_requests) do
+        [
+          spot_instance_request
+        ]
+      end
+
+      let(:current_time) do
+        Time.parse("2015-10-20 12:34:56 UTC")
+      end
+
+      let(:active_points) do
+        [
+          [current_time, 1]
+        ]
+      end
+
+      before do
+        allow_any_instance_of(Dogapi::Client).to receive(:emit_point).and_return(nil)
+      end
+
+      it "should call emit_point" do
+        Timecop.freeze(current_time) do
+          expect_any_instance_of(Dogapi::Client).to receive(:emit_points).with(
+            "spotinstance.status.active",
+            active_points
+          )
+          datadog.send_spot_instance_requests(spot_instance_requests)
+        end
       end
     end
   end
